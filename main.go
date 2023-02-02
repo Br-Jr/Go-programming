6de31860c6e93ab3d14ac5a8ce7662a1fa65c988
Bryant <dumasb@umich.edu>
2023-02-01 22:29:22 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,8 @@
+package main //package that go uses for organization
+
+import "fmt" //import is used to import libraries
+
+func main() { //Main package contains a main function which. This is done automatically
+	name := "Lisa"
+	fmt.Println(name)
+}
